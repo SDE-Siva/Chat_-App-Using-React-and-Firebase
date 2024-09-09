b334a42b91d54547b500dd906ca98644b9b69a46
SDE-Siva <journeyofsiva@gmail.com>
2024-09-09 20:53:02 +0530

    feat : Upadate of ChatBox Code

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -12,6 +12,14 @@ const ChatBox = () => {
         </p>
         <img src={assets.help_icon} className="help" alt="" />
       </div>
+      <div className="chat-input">
+        <input type="text" placeholder="Sent a messge" />
+        <input type="file" id="image" accept="image/png, img/jpeg " hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
     </div>
   );
 };
