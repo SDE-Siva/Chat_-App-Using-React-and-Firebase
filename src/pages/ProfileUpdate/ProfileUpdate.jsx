23efbc5c31b0f1547fd638cdbef97bd434365a25
SDE-Siva <journeyofsiva@gmail.com>
2024-10-09 21:55:50 +0530

    Feat

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './ProfileUpdate.css'
+import React from "react";
+import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
   return (
-    <div>ProfileUpdate Page</div>
-  )
-}
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept=".png, .jph, .jpeg" hidden />
+            <img src={assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+          <input type="text" placeholder="Your name" required />
+          <textarea placeholder="write profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img src={assets.logo_icon} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default ProfileUpdate
+export default ProfileUpdate;
